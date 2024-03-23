ca23775282cf7a222a001003f963a4473d0c00f0
cliu268 <cliu268@gmail.com>
2024-03-23 18:37:20 -0400

    hw1 sync

--- hw8/q1.cpp
+++ hw8/q1.cpp
@@ -1,5 +1,6 @@
 // Subordinates
 // https://xjoi.net/contest/5254/problem/1
+// https://www.xinyoudui.com/ac/contest/7740019EE0002E10119DCD/problem/14124
 /*
 Time limit: 1.00 s
 Memory limit: 512 MB
@@ -51,3 +52,36 @@ int main() {
     cout << dp[i]-1 << " ";
   }
 }
+
+// Ev 80% code
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int n; cin >> n;
+//     vector<vector<int>> children(n+1);
+//     vector<int> answers(n+1, 0);
+//     for(int i=0; i<n-1; i++){
+//         int a; cin >> a;
+//         children[a].push_back(i+2);
+//     }
+
+//     queue<int> q;
+//     for(int i=1; i<n+1; i++){
+//         q.push(i);
+//         int ans=-1;
+//         while(!q.empty()){
+//             int a = q.front();
+//             q.pop();
+//             ans++;
+//             for(int j=0; j<children[a].size(); j++){
+//                 q.push(children[a][j]);
+//             }
+//         }
+//         answers[i]=ans;
+//     }
+
+//     for(int i=1; i<n+1; i++){
+//         cout << answers[i] << " ";
+//     }
+// }
